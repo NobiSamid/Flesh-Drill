78b78ad90b89ab0b5c0bd39522d10470edac2081
Sami un nobi samid <67954576+NobiSamid@users.noreply.github.com>
2021-10-04 21:17:41 +0600

    shop added

---
+++ src/components/shop/Product.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import "./Product.css";
+
+const Product = (props) => {
+    console.log(props.product)
+    const { title, image, color, gender, size } = props.product || {};
+    return (
+        <Card>
+  <Card.Header as="h5">For {gender}</Card.Header>
+  <Card.Body>
+    <Card.Title className="cardText">{title}</Card.Title>
+    <div className="horizontal">
+      <div className="cardText">
+        <Card.Text>
+        Color: {color}
+        </Card.Text>
+        <Card.Text>
+        Size: {size}
+        </Card.Text>
+        </div>
+        <div>
+          <img className='imgEdit' src={image} alt="Thumbnail"></img>
+        </div>
+    </div>
+  </Card.Body>
+</Card>
+    );
+};
+
+export default Product;
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Row } from 'react-bootstrap';
 import Footer from '../footer/Footer';
 import NavBar from '../navBar/NavBar';
+import Product from './Product';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+        fetch('./shop.json')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
     return (
         <div>
             <NavBar></NavBar>
-            <h2>Shop here</h2>
+            <Row className="g-4">
+                {
+                    products.map(product=><Product
+                    key={product.id}
+                    product={product}
+                    ></Product>)
+                }
+            </Row>
             <Footer></Footer>
         </div>
     );
