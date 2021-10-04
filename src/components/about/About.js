7f6a1033e3c139fd41e24d2399830aa060da5029
Sami un nobi samid <67954576+NobiSamid@users.noreply.github.com>
2021-10-04 22:56:22 +0600

    about done

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import Footer from '../footer/Footer';
 import NavBar from '../navBar/NavBar';
+import "./About.css"
 
 const About = () => {
     return (
         <div>
             <NavBar></NavBar>
-            <h4>About me</h4>
+            <div>
+            <img src="https://img.redbull.com/images/c_crop,x_0,y_0,h_1080,w_1920/c_fill,w_1010,h_568/q_auto,f_auto/redbullcom/2019/10/15/82bbee23-844f-4576-8274-74f74538b8d2/calisthenics-park-steel-warriors" alt="Calesthenic-cover"></img>
+            <h1>Our Mission</h1>
+            <p>Human body is a wonderfull creature of God. It has so much ability that you can't imagine.Most of us are well aware of the many health benefits of exercise, but finding the discipline to not only start training but to also stick with it is often the hard part.
+             Whether you’re at the beginning of your fitness journey or in desperate need of some motivation to keep going, you’ll find it on these blogs — and in their educational, inspirational, and empowering content. Our main job is to make everyones body look and works good</p>
+            </div>
+            <div>
+                <img src="https://warrioracademyhk.com/wp-content/uploads/2019/07/2018-06-11-PHOTO-00000059.jpg" alt="vission cover" ></img>
+                <h1>Our vission</h1>
+                <p>
+                The world of bodyweight calisthenics is changing. I believe people are getting sick of spending money at the gym and pumping iron the way Arnold and Jack Lalanne used to do. They are looking for alternatives.
+
+Yet is the answer found in doing a bunch of push ups and no-weight squats?
+
+Calisthenics skill-based exercises have grown in popularity. People like Hannibal for King, Denis Minin and Hit Richards have found large followings because they can do planches, handstand push ups and front levers (and of course because they have great physiques).
+
+But is this the extent of bodyweight calisthenics…cranking out a bunch of super challenging moves? Sure its inspiring to see a bunch of dudes in the park doing something like this… but what’s next?
+                </p>
+            </div>
+            <h1>Team Members</h1>
+            <div className="members">
+                <div className="member">
+                    <h5>Sami</h5>
+                </div>
+                <div className="member">
+                    <h5>Nobi</h5>
+                </div>
+                <div className="member">
+                    <h5>Lam</h5>
+                </div>
+                <div className="member">
+                    <h5>Polok</h5>
+                </div>
+                <div className="member">
+                    <h5>Nahid</h5>
+                </div>
+                <div className="member">
+                    <h5>Mormo</h5>
+                </div>
+                <div className="member">
+                    <h5>Hamim</h5>
+                </div>
+                <div className="member">
+                    <h5>Mohaddis</h5>
+                </div>
+            </div>
+           
             <Footer></Footer>
         </div>
     );
